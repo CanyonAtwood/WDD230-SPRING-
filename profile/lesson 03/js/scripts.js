e0224f9139102d4ea581f42287b167c1527a1899
Canyon Atwood <canyon@byui.edu>
2022-05-04 22:16:50 -0600

    Changed files

--- profile/lesson 03/js/scripts.js
+++ profile/lesson 03/js/scripts.js
@@ -1,5 +1,18 @@
-const lastUpdated = document.querySelector('#lastUpdated');
-lastUpdated.textContent = `Last Updated: ${document.lastModified}`
+let thisYear = new Date().getFullYear();
+document.getElementById("year").innerHTML = thisYear;
 
-const footerYear = document.querySelector('#footerYear');
-footerYear.textContent = new Date().getFullYear();
+
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+let savedate = new Date(document.lastModified);
+let month = savedate.toLocaleString("en-US", {month: "long"});
+let date = savedate.getDate();
+let year = savedate.getFullYear();
+let day = savedate.getDay();
+let weekday = weekdays[savedate.getDay()];
+let lastChanged = weekday+", "+date+" "+month+" "+year; 
+
+document.getElementById("updated").innerHTML = "Last Updated: "+ lastChanged;
+
+function toggleMenu() {
+    document.getElementsByClassName("menu")[0].classList.toggle("responsive");
+}
