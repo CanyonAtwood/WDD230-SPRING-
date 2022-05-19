1c0ec6dbad13e0d0db8393588fc0e416bd936952
Canyon Atwood <canyon@byui.edu>
2022-05-18 22:26:22 -0600

    prophets and weather

---
+++ profile/lesson 09/js/article.js
@@ -0,0 +1,17 @@
+fetch('get api')
+.then((response) => response.json())
+.then((jsObject) => {
+    console.log(jsObject);
+    
+        let title = jsObject.title;
+        let explanation = jsObject.explanation;
+    
+        document.getElementById('title').textContent = (title);
+        document.getElementById('explanation').textContent = (explanation);
+
+        const imagesrc = jsObject.url;
+        const desc = jsObject.title;
+        document.getElementById('photo').setAttribute('src', imagesrc);
+        document.getElementById('photo').setAttribute('alt', desc);
+    
+    });
